ae702158904f8672e802515723a1c8067ae91b09
Dusko Ojdanic <dusko.ojdanic@gmail.com>
2020-03-26 14:58:18 +0100

    First card - draft

--- CoEpi/ui/home/HomeViewController.swift
+++ CoEpi/ui/home/HomeViewController.swift
@@ -3,6 +3,7 @@ import UIKit
 class HomeViewController: UIViewController {
     private let viewModel: HomeViewModel
 
+    @IBOutlet weak var stackContainerView: UIView!
     init(viewModel: HomeViewModel) {
         self.viewModel = viewModel
         super.init(nibName: String(describing: Self.self), bundle: nil)
@@ -18,5 +19,17 @@ class HomeViewController: UIViewController {
     }
     @IBAction func debugTapped(_: UIButton) {
         viewModel.debugTapped()
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.stackContainerView.layer.cornerRadius = 10
+        self.stackContainerView.layer.backgroundColor = UIColor(red: 0.98, green: 0.98, blue: 0.98, alpha: 1).cgColor
+        //https://www.hackingwithswift.com/example-code/uikit/how-to-add-a-shadow-to-a-uiview
+        self.stackContainerView.layer.shadowColor = UIColor.black.cgColor
+        self.stackContainerView.layer.shadowOpacity = 1
+        self.stackContainerView.layer.shadowOffset = CGSize(width: 0, height: 3)
+        self.stackContainerView.layer.shadowRadius = 3
     }
 }
